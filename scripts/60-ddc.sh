7c456094934b260373a6763e8e3174c3f6e0544c
Zhiguang Wang <wangzhiguang@kingsoft.com>
2018-11-29 11:13:23 +0800

    Add script: 60-ddc.sh
    This script auto-mounts Unreal Engine Shared DDC.

---
+++ scripts/60-ddc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+readonly DDC_ENDPOINT="ddc.intranet.rog2.org/ddc"
+readonly MOUNT_POINT="/mnt/ddc"
+
+function idpt_append {
+  local readonly line="$1"
+  local readonly file="$2"
+  if  ! grep -q "$line" "$file" ; then
+    echo "$line" | sudo tee -a "$file"
+  fi
+}
+
+sudo mkdir -p "$MOUNT_POINT"
+
+idpt_append "/-  auto_ddc" "/etc/auto_master"
+
+sudo tee "/etc/auto_ddc" << EOF
+# Unreal Engine Shared DDC
+$MOUNT_POINT  -fstype=smbfs,soft,noowners,nosuid,rw  ://guest:@$DDC_ENDPOINT
+EOF
+
+sudo automount -cv
