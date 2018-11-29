26ab19f34a318c4147533ada1b34cd8ccb632479
Zhiguang Wang <wangzhiguang@kingsoft.com>
2018-11-29 10:27:07 +0800

    Install crudini with pip2.

--- scripts/50-pkgs.sh
+++ scripts/50-pkgs.sh
@@ -12,3 +12,6 @@ readonly BREW_PKG_LIST=(
 readonly BREW_PKGS=$(IFS=' '; echo "${BREW_PKG_LIST[*]}")
 
 brew install $BREW_PKGS
+
+# crudini is not Python3 compatible, so we must install with pip2
+pip2 install --user crudini
