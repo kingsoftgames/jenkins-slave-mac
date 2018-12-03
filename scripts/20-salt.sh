c2d77f5ae38fd2fd3b13b7a935486049831be16b
Zhiguang Wang <wangzhiguang@kingsoft.com>
2018-12-03 10:39:10 +0800

    Workaround issue that $PATH of salt-minion lacks /usr/local/bin
    https://github.com/saltstack/salt/issues/49348

--- scripts/20-salt.sh
+++ scripts/20-salt.sh
@@ -14,7 +14,7 @@ function install_salt {
   local readonly version=$1
   local readonly pkg_name="salt-${version}-py3-x86_64.pkg"
   local readonly dest_pkg_path="/tmp/${pkg_name}"
-  # Downloaded from https://repo.saltstack.com/osx/
+  # Downloaded from https://repo.saltstack.com/#osx
   # Documentations at https://docs.saltstack.com/en/latest/topics/installation/osx.html
   local readonly download_url="https://s3.intranet.rog2.org/software/salt/${pkg_name}"
 
@@ -25,6 +25,14 @@ function install_salt {
 function configure_salt_minion {
   local readonly master=$1
   local readonly minion_id=$(hostname)
+
+  # Workaround issue that $PATH of salt-minion lacks /usr/local/bin
+  # https://github.com/saltstack/salt/issues/49348
+  sudo tee "/opt/salt/bin/start-salt-minion.sh" << EOF
+#!/bin/bash
+export PATH="/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
+exec /opt/salt/bin/salt-minion \$@
+EOF
 
   sudo salt-config --master "$master" --minion-id "$minion_id"
 }
