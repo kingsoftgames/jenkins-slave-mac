b30aae660746c7cf91dd696ceeb2b477a91e9705
Zhiguang Wang <wangzhiguang@kingsoft.com>
2018-12-06 13:20:09 +0800

    Prefer calling python2 to avoid errors.

--- scripts/50-pkgs.sh
+++ scripts/50-pkgs.sh
@@ -16,4 +16,4 @@ readonly BREW_PKGS=$(IFS=' '; echo "${BREW_PKG_LIST[*]}")
 /usr/local/bin/brew install $BREW_PKGS
 
 # crudini is not Python3 compatible, so we must install with pip2
-/usr/local/bin/pip2 install --user crudini
+/usr/local/bin/python2 -m pip install --user crudini
