b1071f71bb411b0992babe2af4bb630f91463f45
Zhiguang Wang <wangzhiguang@kingsoft.com>
2019-03-20 16:37:57 +0800

    Allow user to debug with Xcode without entering admin password.

--- scripts/00-xcode.sh
+++ scripts/00-xcode.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 /usr/bin/xcode-select --install
+
+# Enable "Developer mode"
+sudo /usr/sbin/DevToolsSecurity -enable
+# Add "staff" group to "_developer" group,
+# so admin password is not required when debugging with Xcode
+sudo /usr/sbin/dseditgroup -o edit -t group -a staff _developer
